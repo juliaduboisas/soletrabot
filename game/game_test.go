ca1200aa04ade899021d2396accb918395eea01f
Tiago Hudler <tiagohudler@gmail.com>
2026-07-07 17:03:14 -0300

    feat: add letter set

--- game/game_test.go
+++ game/game_test.go
@@ -2,15 +2,38 @@ package game
 
 import (
 	"testing"
+
+	mapset "github.com/deckarep/golang-set/v2"
 )
 
 func TestGameAddWords(t *testing.T) {
-	game := Game{Words: make(map[string]struct{})}
-	game.AddWords("hello", "world")
-	if _, exists := game.Words["hello"]; !exists {
+	// arrange
+	game := Game{Words: mapset.NewSet[string](), Letters: mapset.NewSet('h', 'e', 'l', 'o')}
+
+	// act
+	game.AddWords([]string{"hello", "world"})
+
+	// assert
+	if !game.Words.Contains("hello") {
 		t.Errorf("Expected word 'hello' not found in game words")
 	}
-	if _, exists := game.Words["world"]; !exists {
-		t.Errorf("Expected word 'world' not found in game words")
+	if game.Words.Contains("world") {
+		t.Errorf("'world' should not be in words list")
+	}
+}
+
+func TestWordValidation(t *testing.T) {
+	// arrange
+	game := Game{Words: mapset.NewSet[string](), Letters: mapset.NewSet('h', 'e', 'l', 'o')}
+
+	// assert
+	if !game.isValidWord("hello") {
+		t.Errorf("'hello' should be a valid word")
+	}
+	if game.isValidWord("he") {
+		t.Errorf("'he' shouldn't be a valid word, contains less than 3 letters")
+	}
+	if !game.isValidWord("hell") {
+		t.Errorf("'hell' should be a valid word")
 	}
 }
